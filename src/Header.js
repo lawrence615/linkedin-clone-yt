3a09de80d06cc1f68533465cf263f3cf6a1a409e
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-27 03:19:39 +0300

    Some work has started on the header, left side with linkedin icon and search input

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
     <div className='header'>
-        <h1>This is the Header</h1>
+        <div className='header__left'>
+            <img src='https://cdn-icons-png.flaticon.com/512/3536/3536505.png' alt=''/>
+            <div className='header__search'>
+                <SearchIcon/>
+                <input type='text'/>
+            </div>
+        </div>
+
+        <div className='header__right'></div>
     </div>
   )
 }
