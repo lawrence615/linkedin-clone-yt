4a7d62b5449575b2402ec83b5fd11ad21c10d89b
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-27 04:17:25 +0300

    added avatar and code identation

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
-    <div className='header'>
-        <div className='header__left'>
-            <img src='https://cdn-icons-png.flaticon.com/512/3536/3536505.png' alt=''/>
-            <div className='header__search'>
-                <SearchIcon/>
-                <input type='text'/>
-            </div>
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
         </div>
+      </div>
 
-        <div className='header__right'></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https:://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
+          title="Me"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
