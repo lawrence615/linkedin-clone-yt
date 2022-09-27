1f70c9050d266fc170f195d6aeeff571adc64dc7
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-27 04:45:31 +0300

    First version of Header is done. On to the next parts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react'; 
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      
-      {/* Header */}
-      <Header/>
+      <Header />
 
       {/* Body */}
       {/* Sidebar */}
