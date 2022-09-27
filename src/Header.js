5c67c1a142f96e53a4888909cb941a770176a07a
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-27 08:03:11 +0300

    moved location of Header.css import

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
@@ -7,6 +6,8 @@ import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ChatIcon from "@mui/icons-material/Chat";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import HeaderOption from "./HeaderOption";
+
+import "./Header.css";
 
 function Header() {
   return (
