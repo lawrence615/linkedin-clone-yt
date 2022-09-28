32e7f88e254e87036ae3679caa426d38d4cb09d6
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-28 08:42:34 +0300

    starting the feed section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
-        {/* Feed */}
+        <Feed/>
         {/* Widgets */}
       </div>
     </div>
