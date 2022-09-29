fb3e19d2381e953e5376efeab4ea18c37cc9f03e
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-29 07:19:12 +0300

    added form

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import CreateIcon from "@mui/icons-material/Create";
 import './Feed.css'
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <div className='feed'>
+        <div className='feed__inputContainer'>
+            <div className='feed__input'>
+                <CreateIcon/>
+                <form>
+                    <input type="text"/>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
