434a42a72c9d52e461c5abf363eb935bd240125c
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-29 08:33:18 +0300

    added InputOption

--- src/Feed.js
+++ src/Feed.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
 import CreateIcon from "@mui/icons-material/Create";
-import './Feed.css'
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarIcon from "@mui/icons-material/CalendarViewDay";
+
+import InputOption from "./components/InputOption";
+import "./Feed.css";
 
 function Feed() {
   return (
-    <div className='feed'>
-        <div className='feed__inputContainer'>
-            <div className='feed__input'>
-                <CreateIcon/>
-                <form>
-                    <input type="text"/>
-                    <button type="submit">Send</button>
-                </form>
-            </div>
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
         </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#COCBCD" />
+          <InputOption Icon={CalendarIcon} title="Write article" color="#7FC15E" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
