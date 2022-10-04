8058e7607cc40be641f35557582f6ad80f60d85e
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-04 09:00:54 +0300

    Checking if user is logged in to determine which views to render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed/>
-        <Widgets/>
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
     </div>
   );
 }
