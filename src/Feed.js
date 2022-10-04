b5e4cc8116316057192062d38e2aa8a07f722ae1
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-04 06:04:25 +0300

    continuous development
    - adding posts to firebase
    - fetching the posts dynamically from firebase
    - ordering the posts by timestamp

--- src/Feed.js
+++ src/Feed.js
@@ -1,31 +1,112 @@
-import React from "react";
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from "react";
 import CreateIcon from "@mui/icons-material/Create";
 import ImageIcon from "@mui/icons-material/Image";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarIcon from "@mui/icons-material/CalendarViewDay";
+import { serverTimestamp } from "firebase/firestore";
 
 import InputOption from "./components/InputOption";
+import Post from "./components/Post";
+import { db, collection, addDoc, getDocs, query, orderBy, onSnapshot } from "./fb";
 import "./Feed.css";
 
 function Feed() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //    async function fetchPosts() {
+  //     const querySnapshot = await getDocs(collection(db, "posts"));
+  //     setPosts(querySnapshot.docs.map((doc) => ({id: doc.id, data: doc.data()})))
+  //   }
+  //   fetchPosts();
+  // }, []); // Or [] if effect doesn't need props or state
+
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy('timestamp', 'desc'));
+    onSnapshot(q, (querySnapshot) => {
+      // const data = [];
+      // querySnapshot.forEach((doc) => {
+      //   // console.log(doc.data())
+      //   data.push({ id: doc.id, data: doc.data() });
+      // });
+      // console.log("data", data);
+      // setPosts(data);
+
+      setPosts(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+      );
+
+      // console.log("Current cities in CA: ", cities.join(", "));
+    });
+  }, []);
+
+  const sendPost = (e) => {
+    // console.log("input", input);
+    e.preventDefault();
+
+    try {
+      const docRef = addDoc(collection(db, "posts"), {
+        name: "Peter Boxxe",
+        description: "This is a test",
+        message: input,
+        photoUrl: "",
+        timestamp: serverTimestamp(),
+      });
+      setInput('')
+      // console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      // console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
           <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
           <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
           <InputOption Icon={EventNoteIcon} title="Event" color="#COCBCD" />
-          <InputOption Icon={CalendarIcon} title="Write article" color="#7FC15E" />
+          <InputOption
+            Icon={CalendarIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          id={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
+      {/* {posts.map(({ id, data: { name, description, message, photoUrl } }) => {
+        <Post
+          id={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />;
+      })} */}
     </div>
   );
 }
