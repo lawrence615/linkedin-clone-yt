dc40c070878a0674425f9689786f1201c609c4a8
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-05 20:19:09 +0300

    replaced static details with current user details

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,12 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const user = useSelector(selectUser)
+
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -17,9 +21,9 @@ function Sidebar() {
           src="https://images.unsplash.com/photo-1658679685433-403db57d289f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
           alt=""
         />
-        <Avatar className="sidebar__avatar" />
-        <h2>Peter Boxxe</h2>
-        <h4>boxxe.peter@mail.com</h4>
+        <Avatar src={user.photoUrl} className="sidebar__avatar">{user.email[0]}</Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
