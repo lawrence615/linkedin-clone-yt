2309ee32c486ff4bc619cf06f4460f813d812933
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-05 08:38:33 +0300

    added styling to the Login page...It now looks like a LinkedIn login page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
 
 function Login() {
+
+  const onLogin = () => {};
+
+  const onRegister = () => {};
+
   return (
-    <div className='login'>Login</div>
-  )
+    <div className="login">
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=lR0Vks"
+        alt=""
+      />
+      <form>
+        <input placeholder="Full name (required if registering)" type="text" />
+        <input placeholder="Profile pic URL (optional)" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+
+        <button type="submit" onClick={onLogin}>Sign In</button>
+      </form>
+      <p>
+        Not a member? {" "}
+        <span className="login__register" onClick={onRegister}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
