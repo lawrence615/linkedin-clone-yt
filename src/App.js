e2091b62cc28df1ab1834f23cada8e1a0cc5e495
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-05 12:01:32 +0300

    Checking whether user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import "./App.css";
-import { selectUser } from "./features/userSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { auth, onAuthStateChanged } from "./fb";
+import { login, logout, selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
 import Login from "./Login";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        dispatch(
+          login({
+            email: user.email,
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoURL,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(logout());
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
