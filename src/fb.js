f534724f4c793e01267aa47a520b2ac99509a9ef
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-07 13:26:25 +0300

    firebase app configuration values fetched from env

--- src/fb.js
+++ src/fb.js
@@ -6,12 +6,12 @@ import { getFirestore, collection, getDocs, addDoc, query, orderBy, onSnapshot }
 import {getAuth, createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX8wf3xhjbZIAf_LLuvUtkPbtdI3xozXs",
-  authDomain: "linkedin-clone-yt-751bc.firebaseapp.com",
-  projectId: "linkedin-clone-yt-751bc",
-  storageBucket: "linkedin-clone-yt-751bc.appspot.com",
-  messagingSenderId: "175674726771",
-  appId: "1:175674726771:web:7d8dc35a0394ac91de8d06",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
