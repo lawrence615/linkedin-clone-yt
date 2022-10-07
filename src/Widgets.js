39b1544fb0022914faf935b4e8e4e8082f7ff389
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-07 09:30:05 +0300

    Update Widgets.js

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import InfoIcon from "@mui/icons-material/Info";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
+import "./Widgets.css";
 
 function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
   return (
-    <div className='widgets'>Widgets</div>
-  )
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle("PAPA React is back", "Top news - 9099 readers")}
+      {newsArticle("Coronavirus: UK updates", "Top news - 886 readers")}
+      {newsArticle("Tesla hits new highs", "Top news - 300 readers")}
+      {newsArticle("Bitcoin Breaks $22k", "Top news - 8000 readers")}
+      {newsArticle("Is Redux too good", "Top news - 123 readers")}
+      {newsArticle("PAPA react launches course?!", "Top news - 6503 readers")}
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
